cc616802e1f622d5d68d53893010dc170d49cd13
Callum <callum@lukso.io>
2022-07-28 16:15:59 +0200

    Add chainId to constants

--- constants.ts
+++ constants.ts
@@ -4,3 +4,4 @@ export const RELAYER_PK = "0x...";
 export const UP_ADDRESS = "0x...";
 export const UP_CONTROLLER_PK = "0x...";
 export const RPC_URL = "https://rpc.l16.lukso.network";
+export const CHAIN_ID = 2828;
